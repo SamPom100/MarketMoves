ab6aca41f0743ced4362e3a90d8bc8d0b837960d
Sam Pomerantz <sam@sampomerantz.me>
2025-03-23 19:08:43 -0400

    added implied moves example

---
+++ examples/implied_moves.py
@@ -0,0 +1,13 @@
+from autohedge import Stock
+
+print("VOO Implied Moves")
+voo = Stock("VOO")
+voo_moves = voo.get_expected_moves()
+for move in voo_moves:
+    print(f"{move}: {voo_moves[move]}%")
+
+print("\n\nMSFT Implied Moves")
+msft = Stock("MSFT")
+msft_moves = msft.get_expected_moves()
+for move in msft_moves:
+    print(f"{move}: {msft_moves[move]}%")
