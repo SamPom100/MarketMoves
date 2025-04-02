5e8ffb041eba3a82e65540773bf43e7d5f0babf6
Sam Pomerantz <sam@sampomerantz.me>
2025-04-02 00:57:25 -0400

    butterfly progress

--- autohedge/marketdata/stock.py
+++ autohedge/marketdata/stock.py
@@ -36,6 +36,17 @@ class Stock(Ticker):
         strangle = self.get_expected_moves_strangle()
         return {date: round((straddle[date] + strangle[date]) / 2, 2) for date in strangle}
     
+    def get_butterfly_all(self, date: str):
+        calls = self.get_calls(date)
+        strikes = calls["strike"].tolist()
+        butterflies = {}
+        for i in range(1, len(strikes) - 1, 1):
+            try:
+                butterflies[strikes[i]] = self.__get_butterfly(date, strikes[i - 1], strikes[i], strikes[i + 1])
+            except ValueError:
+                butterflies[strikes[i]] = None
+        return butterflies
+    
     def __get_midprice(self, option_chain):
         return round(float((option_chain["ask"] + option_chain["bid"]) / 2), 2)
 
@@ -75,3 +86,31 @@ class Stock(Ticker):
         price = self.get_current_price()
         
         return round(100 * ((straddle_value + strangle_value) / 2) / price, 2)
+
+    def __get_butterfly(self, date: str, strike1, strike2, strike3):
+        calls = self.get_calls(date)
+        strikes = calls["strike"].tolist()
+
+        if not (strike1 in strikes and strike2 in strikes and strike3 in strikes):
+            print("Strikes: \n" + str(strikes))
+            raise ValueError("Strikes must be in the option chain")
+        strike1_index = strikes.index(strike1)
+        strike2_index = strikes.index(strike2)
+        strike3_index = strikes.index(strike3)
+        if not (strike1_index + 1 == strike2_index and strike2_index + 1 == strike3_index):
+            print("Strikes: \n" + str(strikes))
+            raise ValueError("Strikes must be next to each other")
+        if not (strike2 - strike1 == strike3 - strike2):
+            raise ValueError("Strike values must be equidistant")
+        if calls[calls["strike"] == strike1]["volume"].iloc[0] < 10:
+            raise ValueError(f"Strike {strike1} Volume must be at least 10")
+        if calls[calls["strike"] == strike2]["volume"].iloc[0] < 10:
+            raise ValueError(f"Strike {strike2} Volume must be at least 10")
+        if calls[calls["strike"] == strike3]["volume"].iloc[0] < 10:
+            raise ValueError(f"Strike {strike3} Volume must be at least 10")
+        
+        one_longcall = self.__get_midprice(calls[calls["strike"] == strike1].iloc[0])
+        two_shortcall = self.__get_midprice(calls[calls["strike"] == strike2].iloc[0])
+        second_longcall = self.__get_midprice(calls[calls["strike"] == strike3].iloc[0])
+
+        return round((one_longcall - (2 * two_shortcall) + second_longcall), 2)
