6483f74db7acde400e8c63105115a9476e94db98
Sam Pomerantz <sam@sampomerantz.me>
2025-04-02 00:28:03 -0400

    small improvements

--- autohedge/marketdata/stock.py
+++ autohedge/marketdata/stock.py
@@ -21,12 +21,23 @@ class Stock(Ticker):
         else:
             self.option_chain_cache[date] = super().option_chain(date)
             return self.option_chain_cache[date].puts
-
-    def __get_midprice(self, option_chain):
-        return (option_chain["ask"] + option_chain["bid"]) / 2
-
+        
     def get_current_price(self):
         return super().info['regularMarketPrice']
+    
+    def get_expected_moves_straddle(self):
+        return {date: self.__get_expected_move_straddle(date) for date in self.get_option_dates()}
+    
+    def get_expected_moves_strangle(self):
+        return {date: self.__get_expected_move_strangle(date) for date in self.get_option_dates()}
+    
+    def get_expected_moves_all(self):
+        straddle = self.get_expected_moves_straddle()
+        strangle = self.get_expected_moves_strangle()
+        return {date: round((straddle[date] + strangle[date]) / 2, 2) for date in strangle}
+    
+    def __get_midprice(self, option_chain):
+        return round(float((option_chain["ask"] + option_chain["bid"]) / 2), 2)
 
     def __get_atm_straddle(self, date: str):
         calls = self.get_calls(date)
@@ -48,19 +59,13 @@ class Stock(Ticker):
         
         return (self.__get_midprice(otm_call), self.__get_midprice(otm_put))
 
-    def __get_expected_move_straddle(self, date: str = None):
-        if date == None:
-            date = self.get_option_dates()[0]
-        
+    def __get_expected_move_straddle(self, date: str):
         call_price, put_price = self.__get_atm_straddle(date)
         price = self.get_current_price()
         
         return round(100 * ((call_price + put_price) * 0.85) / price, 2)
 
-    def __get_expected_move_strangle(self, date: str = None):
-        if date == None:
-            date = self.get_option_dates()[0]
-        
+    def __get_expected_move_strangle(self, date: str):        
         straddle_call, straddle_put = self.__get_atm_straddle(date)
         strangle_call, strangle_put = self.__get_otm_strangle(date)
         
@@ -70,18 +75,3 @@ class Stock(Ticker):
         price = self.get_current_price()
         
         return round(100 * ((straddle_value + strangle_value) / 2) / price, 2)
-
-    def get_expected_moves(self, date: str = None):
-        options_dict = {}
-        if (date == None):
-            for date in self.get_option_dates():
-                straddle = self.__get_expected_move_straddle(date)
-                strangle = self.__get_expected_move_strangle(date)
-                value = (straddle + strangle) / 2
-                options_dict[date] = round(value.item(), 2)
-        else:
-            straddle = self.__get_expected_move_straddle(date)
-            strangle = self.__get_expected_move_strangle(date)
-            value = (straddle + strangle) / 2
-            options_dict[date] = round(value.item(), 2)
-        return options_dict
--- examples/implied_moves.py
+++ examples/implied_moves.py
@@ -2,12 +2,12 @@ from autohedge import Stock
 
 print("VOO Implied Moves")
 voo = Stock("VOO")
-voo_moves = voo.get_expected_moves()
+voo_moves = voo.get_expected_moves_all()
 for move in voo_moves:
     print(f"{move}: {voo_moves[move]}%")
 
 print("\n\nMSFT Implied Moves")
 msft = Stock("MSFT")
-msft_moves = msft.get_expected_moves()
+msft_moves = msft.get_expected_moves_all()
 for move in msft_moves:
     print(f"{move}: {msft_moves[move]}%")
